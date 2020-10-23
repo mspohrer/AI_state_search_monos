0b833c4aeb72a3fd4604c71540b3a35c72743eae
mspohrer <mspohrer@pdx.edu>
2020-10-22 19:39:49 -0700

    2nd commit, works but needs writeup

--- monodfs.py
+++ monodfs.py
@@ -5,35 +5,52 @@
 import sys
 
 
-def dfs(rs, todo, n, ri):
-    # base case, at the last index of rooms
-    if ri == n:
-        # we're done here, we're at the last value and the
-        # last room.
-        if len(todo) == 1:
-            return "unsat"
-        return rs
+def check(rs):
+    for i in rs:
+        for j in i:
+            for k in i:
+                if k != j and j+k in i:
+                    print("j: ", j, "k: ", k, "i: ", i)
+    print("true")
 
-    # get the monopole to add to a room
-    t = todo.pop(0)
 
-    print("t: ", t, "ri: ", ri, "rs[ri]: ", rs[ri])
-    # search the current room to see if the current monopole
-    # can be added to it
-    nflag, i, t2 = 0, 0, t/2
-    while nflag == 0 and i < len(rs[ri]) and rs[i] < t2:
-        to_check = t - rs[i]
-        if to_check in rs[ri] and to_check != rs[i]:
-            nflag = 1
+def pretty_print(rs):
+    i, stop = 0, len(rs)
+    while i < stop:
+        print(*rs[i])
         i += 1
 
-    if nflag == 0:
+
+def dfs(rs, todo, n, ri):
+    if ri == n:
+        return 0
+
+    t = todo.pop(0)
+    # search the current room to see if the current monopole
+    # can be added to it
+    can_add, i, t2 = 1, 0, t/2
+    while can_add == 1 and i < len(rs[ri]) and rs[ri][i] < t2+1:
+        to_check = t - rs[ri][i]
+        if to_check in rs[ri] and to_check != rs[ri][i]:
+            can_add = 0
+        i += 1
+
+    ret = 0
+    if can_add == 1:
         rs[ri].append(t)
-        rs = dfs(rs, todo, n, 0)
+        if len(todo) == 0:
+            return rs
 
-    todo.insert(0, t)
-    return dfs(rs, todo, n, ri + 1)
+        ret = dfs(rs, todo, n, 0)
 
+        if ret == 0:
+            rs[ri].remove(t)
+
+    if ret == 0:
+        todo.insert(0, t)
+        ret = dfs(rs, todo, n, ri+1)
+
+    return ret
 
 
 def main():
@@ -45,8 +62,12 @@ def main():
     todo = [*ms]
 
     rs = dfs(rs, todo, n, 0)
-    print("doneso: ")
-    print(rs)
+    if rs == 0:
+        print("unsat")
+    else:
+        pretty_print(rs)
+
+    check(rs)
 
 
 if __name__ == '__main__':
